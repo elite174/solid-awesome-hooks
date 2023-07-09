9234c0d4416f97277c69151e4566748851fd2839
Vladislav Lipatov <lipatov.xex@gmail.com>
2023-07-09 09:22:38 +0300

    Add an example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import "./App.css";
+import { createSignal } from "solid-js";
+import { useClickOutside } from "./lib";
 
 function App() {
+  const [listeningEnabled, setListeningEnabled] = createSignal(true);
+
+  const setRef = useClickOutside((e) => console.log("Clicked outside: ", e), {
+    enabled: listeningEnabled,
+  });
+
   return (
     <main>
-      <hgroup>
-        <h1>Solid-awesome hooks</h1>
-        <p>A collection of useful hooks for solid-js</p>
-      </hgroup>
+      <h1>Solid awesome hooks</h1>
       <section>
         <h2>useClickOutside</h2>
+        <button ref={setRef} onClick={() => setListeningEnabled((e) => !e)}>
+          Click outside (see the console){" "}
+          {listeningEnabled() ? "enabled" : "disabled"}
+        </button>
       </section>
     </main>
   );
