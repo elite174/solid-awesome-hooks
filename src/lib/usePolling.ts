13f440659049bcdd85e1a3f040fcc4ea4cc3812b
Vladislav Lipatov <lipatov.xex@gmail.com>
2023-10-20 12:14:50 +0700

    Added owner to poll function

--- src/lib/usePolling.ts
+++ src/lib/usePolling.ts
@@ -1,4 +1,4 @@
-import { type Accessor, createEffect, on, onCleanup } from "solid-js";
+import { type Accessor, createEffect, on, onCleanup, type Owner, runWithOwner, getOwner } from "solid-js";
 
 type UsePollingOptions = {
   /**
@@ -7,6 +7,12 @@ type UsePollingOptions = {
    */
   pollingTime?: number;
   enabled?: Accessor<boolean>;
+  /**
+   * This hook uses setTimeout for polling, so it might be the case when `poll` function triggers reactive things.
+   * To make it work correctly pass proper owner for the `poll` function.
+   * Otherwise it will be assigned automatically (the owner of the hook will be used)
+   */
+  owner?: Owner;
 };
 
 const DEFAULT_OPTIONS = {
@@ -21,7 +27,9 @@ const DEFAULT_OPTIONS = {
  * @param options {UsePollingOptions}
  */
 export const usePolling = (readyTrigger: Accessor<unknown>, poll: VoidFunction, options?: UsePollingOptions) => {
-  const { pollingTime, enabled } = Object.assign({}, DEFAULT_OPTIONS, options);
+  const { pollingTime, enabled, owner = getOwner() } = Object.assign({}, DEFAULT_OPTIONS, options);
+
+  const pollWithOwner = () => runWithOwner(owner, () => poll());
 
   // This effect is triggered when the data signal changes
   // Thus, we don't rely on slow network
@@ -29,7 +37,7 @@ export const usePolling = (readyTrigger: Accessor<unknown>, poll: VoidFunction, 
     on([readyTrigger, enabled], ([_, isEnabled]) => {
       if (!isEnabled) return;
 
-      const timer = setTimeout(() => poll(), pollingTime);
+      const timer = setTimeout(() => pollWithOwner(), pollingTime);
 
       onCleanup(() => clearTimeout(timer));
     })
